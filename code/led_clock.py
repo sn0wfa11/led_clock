5a1b09c99bf390a35022d220607faa6f5ddebe5b
Josh Hale <jhale85446@gmail.com>
2019-12-27 18:52:20 -0600

    Testing 4b

--- code/led_clock.py
+++ code/led_clock.py
@@ -122,9 +122,9 @@ def test1(strip):
     color_chase(strip, ORANGE, wait = 0.01, direction = 2)
     color_chase(strip, YELLOW, wait = 0.01)
     color_chase(strip, GREEN, wait = 0.01, direction = 2)
-    color_chase(strip, BLUE, wait = 0.01)
-    color_chase(strip, PURPLE, wait = 0.01, direction = 2)
     color_chase(strip, CYAN, wait = 0.01)
+    color_chase(strip, BLUE, wait = 0.01, direction = 2)
+    color_chase(strip, PURPLE, wait = 0.01)
 
     time.sleep(2)
     close_strip(strip)
